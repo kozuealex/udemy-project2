90bcc31dad38b16a34d85f8f177a700d79c00bf4
Kozue Yamada <kozue@dualdecade.com>
2022-08-06 22:09:59 +0200

    Prog. home 22-08-06

---
+++ UdemyProject2/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                Debug.Log("Congrats, you finished!");
+                break;
+            case "Fuel":
+                Debug.Log("You picked up fuel");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
+   
--- UdemyProject2/Assets/Scripts/Movement.cs
+++ UdemyProject2/Assets/Scripts/Movement.cs
@@ -7,10 +7,12 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rotationThrust = 1f;
     Rigidbody rb;
+    AudioSource audioSource;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -24,6 +26,14 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
